c30ba36e721d40e3e8f462f6b2dc987febc88875
Araviel-IO <contact@araviel.io>
2021-11-02 23:18:23 +0100

    landing layouting

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -13,7 +13,7 @@ import { Color } from '@material-ui/lab/Alert';
 import { useDarkMode } from "../hooks/useDarkMode";
 
 const styles = {
-    smallIcon: {
+  smallIcon: {
     fontSize: "1.4em",
     color: "#0C2533"
   },
@@ -42,60 +42,73 @@ interface Message {
 };
 
 export function Landing() {
-    const [message, setMessage] = useState<Message>({open: false, content: '', severity: 'success'});
-    const [open, setOpen] = useState(false);
-    const [openVideo, setOpenVideo] = useState(false);
-    const [isDark, setisDark] = useState(false);
+  const [message, setMessage] = useState<Message>({ open: false, content: '', severity: 'success' });
+  const [open, setOpen] = useState(false);
+  const [openVideo, setOpenVideo] = useState(false);
+  const [isDark, setisDark] = useState(false);
 
-    var darkmode = useDarkMode();
-    useEffect(() => {
-      
-      if (darkmode[0] === false) {
-        setisDark(false);
-      }
-      else {
-        setisDark(true);
-      }
-    }, []);
+  var darkmode = useDarkMode();
+  useEffect(() => {
 
-    function handleClose() {
-      setOpen(false);
+    if (darkmode[0] === false) {
+      setisDark(false);
     }
-
-    function handleCloseVideo() {
-      setOpenVideo(false);
+    else {
+      setisDark(true);
     }
+  }, []);
 
-    function handleCloseSnackbar() {
-      setMessage({open: false, content: '', severity: 'success'});
-    }
+  function handleClose() {
+    setOpen(false);
+  }
 
-    const classes = useStyles();
-    console.log("is dark ? ", isDark)
-    return (
-      <div id="landing">
-        <div className={classes.root}>
-          <Grid
-            container
-            alignItems="center"
-            justify="center"
-            direction="column"
-            style={{minHeight: '100vh', textAlign: 'center', overflow: 'hidden'}}
-          >
-            <Grid item xs={10}>
-              <div className="flex justify-center text-center p-0">
-                <div className="w-10/12 sm:1/4 md:w-3/4 lg:w-2/3 xl:w-1/3 pt-5 md:pt-0">
-                {/*isDark
+  function handleCloseVideo() {
+    setOpenVideo(false);
+  }
+
+  function handleCloseSnackbar() {
+    setMessage({ open: false, content: '', severity: 'success' });
+  }
+
+  const classes = useStyles();
+  console.log("is dark ? ", isDark)
+  return (
+    <div id="landing">
+      <div className={classes.root}>
+
+        <Grid
+          container
+          alignItems="center"
+          justify="center"
+          direction="column"
+          style={{ minHeight: '100vh', textAlign: 'center', overflow: 'hidden' }}
+        >
+          <div className="flex justify-center text-center p-0">
+            <div>
+              <div className="flex-col justify-center text-center p-0">
+              <div className="flex justify-center text-center pb-16">
+                <div className="w-10/12 sm:1/4 md:w-3/4 lg:w-2/3 xl:w-1/8 pt-5 md:pt-0">
+                  {/*isDark
               ? <SafestakeLogoMainSvgDark />
               : <SafestakeLogoMainSvg />
                 */}
-                <SafestakeLogoMainSvg />
+                  <SafestakeLogoMainSvg />
+                  </div>
                 </div>
+                <div className="flex justify-center text-center pb-24">
+                <Link to="/app">
+                  <div className=" tracking-wider safeBtnInverted">
+                    <span className="text-md sm:text-xl p-2">Stake now</span>
+                  </div>
+                </Link>
               </div>
-              <Typography style={{visibility: 'hidden'}}>
-                  Hack for non working svg scaling SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+              </div>
+              <Typography style={{ visibility: 'hidden' }}>
+                Hack for non working svg scaling SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
               </Typography>
+            </div>
 
+            <div>
               <div className="font-display uppercase text-4half sm:text-5xl md:text-6xl text-solblue-dark dark:text-solblue font-bold leading-tight">
                 <p className="inline px-2 md:block md:px-0">Stake your SAFE</p>
                 <p className="inline px-2 md:block md:px-0">manage accounts</p>
@@ -108,88 +121,89 @@ export function Landing() {
 
               <Box m={4} />
 
-                <div className="flex justify-center text-center pb-24">
-                  <Link to="/app">
-                    <div className="solBtnAcid font-bold">
-                      <span className="text-md sm:text-xl p-2">Use Safestake</span>
-                    </div>
-                  </Link>
-                </div>
-    
 
-              <div className="flex justify-center text-center pb-5">
-                <div className="w-4/6 text-lg text-solblue-darker dark:text-gray-300">We’re just getting started around here. In the meantime stay updated on product releases, new features and more.</div>
-              </div>
-  
-              <div>
-                <IconButton
-                  href="https://github.com/Fair-Exchange/safestake"
-                  rel="noopener noreferrer" target="_blank"
-                >
-                  <GitHub style={styles.smallIcon} />
-                </IconButton>
-                <IconButton
-                  onClick={() => setOpenVideo(true)}
-                >
-                  <YouTube style={styles.largeIcon} />
-                </IconButton>
-                <IconButton
-                  href="https://discord.com/invite/vQgYGJz"
-                  rel="noopener noreferrer" target="_blank"
-                >
-                  <SvgIcon style={styles.smallIcon}>
-                    <Discord />
-                  </SvgIcon>
-                </IconButton>
-                <IconButton
-                  href="https://twitter.com/safecoins"
-                  rel="noopener noreferrer" target="_blank"
-                >
-                  <Twitter style={styles.mediumIcon} />
-                </IconButton>
-              </div>
-            </Grid>
-          </Grid>
-        </div>
-        <Dialog
-          title="Email sent!"
-          fullWidth={true}
-          open={open}
-          onClose={handleClose}
-        >
-          <DialogTitle>Email sent!</DialogTitle>
-          <DialogContent>
-            <Box m={1}>
-              <Typography>
-                Thank you for registering, we will get back to you when SafeStake is production ready
-              </Typography>
-            </Box>
-          </DialogContent>
-  
-          <DialogActions>
-            <Button onClick={handleClose} color="primary">
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Dialog
-          open={openVideo}
-          fullWidth
-          maxWidth="md"
-          onClose={handleCloseVideo}
-        >
-          <div className="videoWrapper">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/JUDG6j5ktW4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
+
           </div>
-        </Dialog>
-        {/* <div id="stars"></div>
+          <Grid item xs={10}>
+
+
+
+
+
+            <div className="flex justify-center text-center pb-5">
+              <div className="w-4/6 text-lg text-solblue-darker dark:text-gray-300">We’re just getting started around here. In the meantime stay updated on product releases, new features and more.</div>
+            </div>
+
+            <div>
+              <IconButton
+                href="https://github.com/Fair-Exchange/safestake"
+                rel="noopener noreferrer" target="_blank"
+              >
+                <GitHub style={styles.smallIcon} />
+              </IconButton>
+              <IconButton
+                onClick={() => setOpenVideo(true)}
+              >
+                <YouTube style={styles.largeIcon} />
+              </IconButton>
+              <IconButton
+                href="https://discord.com/invite/vQgYGJz"
+                rel="noopener noreferrer" target="_blank"
+              >
+                <SvgIcon style={styles.smallIcon}>
+                  <Discord />
+                </SvgIcon>
+              </IconButton>
+              <IconButton
+                href="https://twitter.com/safecoins"
+                rel="noopener noreferrer" target="_blank"
+              >
+                <Twitter style={styles.mediumIcon} />
+              </IconButton>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <Dialog
+        title="Email sent!"
+        fullWidth={true}
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Email sent!</DialogTitle>
+        <DialogContent>
+          <Box m={1}>
+            <Typography>
+              Thank you for registering, we will get back to you when SafeStake is production ready
+            </Typography>
+          </Box>
+        </DialogContent>
+
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog
+        open={openVideo}
+        fullWidth
+        maxWidth="md"
+        onClose={handleCloseVideo}
+      >
+        <div className="videoWrapper">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/JUDG6j5ktW4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        </div>
+      </Dialog>
+      {/* <div id="stars"></div>
         <div id="stars2"></div>
         <div id="stars3"></div> */}
-        <Snackbar open={message.open} autoHideDuration={10000} onClose={handleCloseSnackbar}>
-          <Alert onClose={handleClose} severity={message.severity}>
-            {message.content}
-          </Alert>
-        </Snackbar>
-      </div>
-    );
-  }
+      <Snackbar open={message.open} autoHideDuration={10000} onClose={handleCloseSnackbar}>
+        <Alert onClose={handleClose} severity={message.severity}>
+          {message.content}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
